0a07102337816ceba57459caf246bb56fdbd11c0
Huan Bui <huanbv9x@icloud.com>
2021-08-22 20:48:56 +0700

    add HTML and CSS for the Template

--- src/main/modules/auth/forms/login_form.py
+++ src/main/modules/auth/forms/login_form.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[
-        DataRequired(message='Please fill out the email field'),
-    ],)
+        DataRequired(message='Please fill out the email field'), Email(),
+    ], render_kw={"placeholder": "Email or Username"})
 
     password = PasswordField(label='Password', validators=[
         DataRequired(message='Please fill out the password field'),
-    ],)
+    ], render_kw={"placeholder": "Password"})
 
 
--- src/main/modules/auth/forms/signup_form.py
+++ src/main/modules/auth/forms/signup_form.py
@@ -1,29 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.validators import DataRequired, EqualTo, Length, Email
 
 
 class SignupForm(FlaskForm):
     email = StringField(label='Email', validators=[
-        DataRequired(message='Please fill out the email field'),
-    ],)
+        DataRequired(message='Please fill out the email field'), Email(),
+    ], render_kw={"placeholder": "Email"})
 
     first_name = StringField(label='First name', validators=[
         Length(max=30, message='Maximum 30 letters'),
         DataRequired(message='Please fill out the first name field')
-    ])
+    ], render_kw={"placeholder": "First Name"})
 
-    last_name = StringField(label='First name', validators=[
+    last_name = StringField(label='Last name', validators=[
         Length(max=20, message='Maximum 20 letters'),
         DataRequired(message='Please fill out the first name field')
-    ])
+    ], render_kw={"placeholder": "Last Name"})
 
     password = PasswordField(label='Password', validators=[
         DataRequired(message='Please fill out the password field')
-    ])
+    ], render_kw={"placeholder": "Password"})
 
     re_password = PasswordField(label='Re-enter the password', validators=[
         EqualTo('password', message='The password do not match'),
         DataRequired(message='Please fill out all the password fields')
-    ])
+    ], render_kw={"placeholder": "Re-Enter The Password"})
 
